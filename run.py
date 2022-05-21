b311a5a8c164238685b95550c8e77a6da8b68bd6
matt70iu <mattadams60@gmail.com>
2022-05-21 12:08:54 +0000

    initial commit, set up player and oponent dictionaries and test with print

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+player_name = 'Matt'
+player_attack = 10
+player_heal = 16
+health = 100
+
+player = {'name': 'Matt', 'attack': 10, 'heal': 16, 'health': 100}
+opponent = {'name': 'Max', 'attack': 12, 'health': 100}
+
+print('Please select action')
+print('1) Attack')
+print('2) Heal')
+
+
+
