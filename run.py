b83b2d780763f821fe90ddf097cf5147f2626d32
matt70iu <mattadams60@gmail.com>
2022-05-21 13:42:15 +0000

    correct viriable capitalisation

--- run.py
+++ run.py
@@ -1,25 +1,28 @@
-player_name = 'Matt'
-player_attack = 10
-player_heal = 16
-health = 100
+Player_Name = 'Matt'
+Player_Attack = 10
+Player_Health = 16
+Health = 100
 
-player = {'name': 'Matt', 'attack': 10, 'heal': 16, 'health': 100}
-opponent = {'name': 'Max', 'attack': 12, 'health': 100}
-game_in_progress = True
+Player = {'name': 'Matt', 'attack': 10, 'heal': 16, 'health': 100}
+Opponent = {'name': 'Max', 'attack': 12, 'health': 100}
+Game_In_Progress = True
 
-while game_in_progress == True:
+while Game_In_Progress == True:
 
     print('Please select action')
     print('1) Attack')
     print('2) Heal')
 
-    player_selection = input()
-    if player_selection == '1':
-        opponent['health'] = opponent['health'] - player['attack']
-        player['health'] = player['health'] - opponent['attack']
-        print(opponent['health'])
-        print(player['health'])
-    elif player_selection == '2':
+    Player_Selection = input()
+    if Player_Selection == '1':
+        Opponent['health'] = Opponent['health'] - Player['attack']
+        Player['health'] = Player['health'] - Opponent['attack']
+        print(Opponent['health'])
+        print(Player['health'])
+    elif Player_Selection == '2':
         print('Heal player')
     else:
         print('invalid Input')
+
+        if Player['health'] <= 0:
+            Game_In_Progress = False
